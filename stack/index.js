3d2c9165d45c43091bcc8d5093d8fa4408cea96c
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-04-14 18:04:32 +0200

    Update index.js

--- stack/index.js
+++ stack/index.js
@@ -1,10 +1,1 @@
-function boh(datastore, ...data) {
-    for (i = 0; i < data.length; i++) {
-        datastore[i] = data[i]
-    }
 
-
-}
-let datastore = [];
-boh(datastore, 1, 2, 3)
-console.log(datastore)
